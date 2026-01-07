110567da695765dd45f6e278eb6cc8d8fde1f45a
Azeezfasasi <fas.azeez@gmail.com>
2026-01-07 16:00:35 +0100

    updated footer

--- src/components/home-component/Footer.js
+++ src/components/home-component/Footer.js
@@ -62,8 +62,8 @@ export default function Footer() {
               </Link>
             </li>
             <li>
-              <Link href="/projects" className="hover:text-white transition">
-                Projects
+              <Link href="/track-shipment" className="hover:text-white transition">
+                Track Shipment
               </Link>
             </li>
             <li>
@@ -78,14 +78,13 @@ export default function Footer() {
         <div>
           <h4 className="text-white text-lg font-semibold mb-4">Our Services</h4>
           <ul className="space-y-3">
-            <li>Civil Engineering</li>
-            <li>Mechanical Works</li>
-            <li>Electrical Installations</li>
-            <li>Project Management</li>
-            <li>Consultancy</li>
-            <li>Telecom Services</li>
-            <li>Optical Fibre Implementation/Operations</li>
-            <li>Maintenance</li>
+            <li>Pick-Up Service</li>
+            <li>Customs Clearing</li>
+            <li>Export/Import</li>
+            <li>Door-to-Door Delivery</li>
+            <li>Consolidated Air Freight</li>
+            <li>Sea Cargo</li>
+            <li>More...</li>
           </ul>
         </div>
 
@@ -94,26 +93,26 @@ export default function Footer() {
           <h4 className="text-white text-lg font-semibold mb-4">Contact Info</h4>
           <ul className="space-y-4 text-sm">
             <li className="flex items-start gap-3">
-              <MapPin size={18} className="text-blue-500 mt-1" />
+              <MapPin size={18} className="text-amber-600 mt-1" />
               <span>
-                31/35 Ebute kekere way,
-                <br /> Akesan Lagos.
+                7534 Southlake Parkway, Jonesboro, <br />
+                Atlanta, Georgia, 30236, USA
               </span>
             </li>
             <li className="flex items-center gap-3">
-              <Phone size={18} className="text-blue-500" />
-              <a href="tel:+2348152260336" className="hover:text-white transition">
-                (+234) 08152260336
+              <Phone size={18} className="text-amber-600" />
+              <a href="tel:+14705040841" className="hover:text-white transition">
+                +1 470 504 0841, 
               </a>
             </li>
             <li className="flex items-center gap-3">
-              <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6.014 8.00613C6.12827 7.1024 7.30277 5.87414 8.23488 6.01043L8.23339 6.00894C9.14051 6.18132 9.85859 7.74261 10.2635 8.44465C10.5504 8.95402 10.3641 9.4701 10.0965 9.68787C9.7355 9.97883 9.17099 10.3803 9.28943 10.7834C9.5 11.5 12 14 13.2296 14.7107C13.695 14.9797 14.0325 14.2702 14.3207 13.9067C14.5301 13.6271 15.0466 13.46 15.5548 13.736C16.3138 14.178 17.0288 14.6917 17.69 15.27C18.0202 15.546 18.0977 15.9539 17.8689 16.385C17.4659 17.1443 16.3003 18.1456 15.4542 17.9421C13.9764 17.5868 8 15.27 6.08033 8.55801C5.97237 8.24048 5.99955 8.12044 6.014 8.00613Z" fill="#2196F3"></path> <path fillRule="evenodd" clipRule="evenodd" d="M12 23C10.7764 23 10.0994 22.8687 9 22.5L6.89443 23.5528C5.56462 24.2177 4 23.2507 4 21.7639V19.5C1.84655 17.492 1 15.1767 1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23ZM6 18.6303L5.36395 18.0372C3.69087 16.4772 3 14.7331 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C11.0143 21 10.552 20.911 9.63595 20.6038L8.84847 20.3397L6 21.7639V18.6303Z" fill="#2196F3"></path> </g></svg>
-              <a href="https://wa.me/08030960533" target="_blank" className="hover:text-white transition">
-                (+234) 08030960533
+              <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6.014 8.00613C6.12827 7.1024 7.30277 5.87414 8.23488 6.01043L8.23339 6.00894C9.14051 6.18132 9.85859 7.74261 10.2635 8.44465C10.5504 8.95402 10.3641 9.4701 10.0965 9.68787C9.7355 9.97883 9.17099 10.3803 9.28943 10.7834C9.5 11.5 12 14 13.2296 14.7107C13.695 14.9797 14.0325 14.2702 14.3207 13.9067C14.5301 13.6271 15.0466 13.46 15.5548 13.736C16.3138 14.178 17.0288 14.6917 17.69 15.27C18.0202 15.546 18.0977 15.9539 17.8689 16.385C17.4659 17.1443 16.3003 18.1456 15.4542 17.9421C13.9764 17.5868 8 15.27 6.08033 8.55801C5.97237 8.24048 5.99955 8.12044 6.014 8.00613Z" fill="#FFBF00"></path> <path fillRule="evenodd" clipRule="evenodd" d="M12 23C10.7764 23 10.0994 22.8687 9 22.5L6.89443 23.5528C5.56462 24.2177 4 23.2507 4 21.7639V19.5C1.84655 17.492 1 15.1767 1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23ZM6 18.6303L5.36395 18.0372C3.69087 16.4772 3 14.7331 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C11.0143 21 10.552 20.911 9.63595 20.6038L8.84847 20.3397L6 21.7639V18.6303Z" fill="#FFBF00"></path> </g></svg>
+              <a href="https://wa.me/14705040841" target="_blank" className="hover:text-white transition">
+                +1 470 504 0841,
               </a>
             </li>
             <li className="flex items-center gap-3">
-              <Mail size={18} className="text-blue-500" />
+              <Mail size={18} className="text-amber-600" />
               <a href="mailto:info@modestcargo.com" className="hover:text-white transition">
                 info@modestcargo.com
               </a>
@@ -125,7 +124,7 @@ export default function Footer() {
       {/* Bottom Bar */}
       <div className="border-t border-gray-700 mt-12 pt-6 text-center text-sm text-gray-500">
         © {new Date().getFullYear()} <span className="text-gray-300 font-medium">Modest Cargo</span>. 
-        All rights reserved. | Developed by <a href="https://wa.me/2348117256648" target="_blank" className="text-blue-500">Sense Solutions</a>
+        All rights reserved. | Developed by <a href="https://wa.me/2348117256648" target="_blank" className="text-red-600">Sense Solutions</a>
       </div>
     </footer>
   );
